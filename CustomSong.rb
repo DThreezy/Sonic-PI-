13af3737d110a722a979962355bcdbd0590b4514
DThreezy <160058020+DThreezy@users.noreply.github.com>
2024-02-29 11:25:07 -0800

    Update CustomSong.rb

--- CustomSong.rb
+++ CustomSong.rb
@@ -1,51 +1,93 @@
-use_bpm 120
+#Custom Song My Girl By: Temptations
 
-live_loop :suspensful_till_beat_drop do
-  1.times do
-    play :D4, release: 5
-    sleep 5
-    play :D4, release: 5
-    sleep 5
-    play :F4
-    sleep 0.5
-    play :F4
-    sleep 0.5
-    play :A4
-    sleep 0.5
-    play :F4
-    sleep 0.5
-    play :E4
-    sleep 0.5
-    play :D4
-    sleep 0.5
-  end
-  stop
-end
+#https://www.youtube.com/watch?v=KLcMBBC55kA
+intro = "C:/Users/deon_radcliff/Documents/Audacity/intropart.wav"
 
-sleep 13
-live_loop :no_suspense do
-  play :D4
-  sleep 0.5
-  play :D4
-  sleep 0.5
-  play :F4
-  sleep 0.5
-  play :F4
-  sleep 0.5
-  play :A4
-  sleep 0.5
-  play :F4
-  sleep 0.5
-  play :E4
-  sleep 0.5
-  play :D4
-  sleep 0.5
-end
+second = "C:/Users/deon_radcliff/Documents/Audacity/secondpart.wav"
 
-sleep 15
-live_loop :amen_break do
-  sample :loop_amen, beat_stretch: 2
-  sleep 4
-end
+outro = "C:/Users/deon_radcliff/Documents/Audacity/youtuber.wav"
 
 
+
+
+
+
+
+
+use_synth :fm
+
+play :G3
+sleep 0.5
+
+play :C3
+sleep 0.5
+
+play :C3, release: 2
+sleep 1
+
+play :G3
+sleep 0.5
+
+play :C3
+sleep 0.5
+
+play :C3, release: 2
+sleep 1
+
+play :G3
+sleep 0.5
+
+play :C3
+sleep 0.5
+
+play :C3, release: 2
+sleep 1
+
+play :G3
+sleep 0.5
+
+play :C3
+sleep 0.5
+
+play :C3, release: 2
+sleep 1
+
+
+sample intro, amp: 3, release: 5
+sleep 1
+
+
+sleep 22.75
+play :b3, release: 3, amp: 0.5
+sleep 1.5
+play :b4, release: 3, amp: 0.5
+sleep 1.5
+play :b5, release: 3, amp: 0.5
+sleep 1.5
+play :b5, release: 1, amp: 0.5
+sleep 1.5
+
+
+play :b4
+sleep 0.75
+play :b4
+sleep 0.75
+play :b4
+sleep 0.75
+play :a5
+sleep 0.75
+play :a4
+sleep 0.75
+play :a4
+sleep 0.75
+play :a4, release: 2
+sleep 0.75
+
+
+sample second, amp: 3, release: 5
+sleep 1
+
+sleep 40
+
+sample outro, amp: 3, release: 5
+sleep 1
